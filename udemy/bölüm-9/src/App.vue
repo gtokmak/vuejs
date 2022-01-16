e1656b9776423b6a33fc3c4ace6f8ea42c87a73a
gtokmak <gokhantkmk@gmail.com>
2022-01-16 13:38:11 +0300

    bölüm-9

---
+++ udemy/bölüm-9/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <br><br>
+        <button class="btn-sm btn-primary" @click="selectedComponent = 'appMemory'"> appMemory</button>
+        <button class="btn-sm btn-primary" @click="selectedComponent = 'appNewMemory'"> appNewMemory</button>
+        <button class="btn-sm btn-primary" @click="selectedComponent = 'appAuthor'"> appAuthor</button>
+        <hr>
+        <p> {{ selectedComponent }} </p>
+        <hr>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <h1 slot="heading">Güzel Bir Gün</h1>
+            <p >Bu çok güzel bir anı...</p>
+          </component>
+        </keep-alive>
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Memory from "./components/Memory.vue"
+import NewMemory from "./components/NewMemory.vue"
+import Author from "./components/Author.vue"
+
+export default {
+  components:{
+    appMemory : Memory,
+    appNewMemory : NewMemory,
+    appAuthor : Author
+  },
+  data() {
+    return {
+      selectedComponent : "appMemory"
+    }
+  },
+  
+}
+</script>
+
---
+++ udemy/bölüm-9/src/components/NewMemory.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+         <h1>NewMemory Componenti</h1>
+         <button @click="counter++">Arttır</button>
+         <p> {{ counter }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+     data() {
+         return {
+             counter : 0
+         }
+     },
+     destroyed() {
+         console.log("destroyed");
+     },
+     deactivated() {
+         console.log("deactivated")
+     },
+     activated() {
+         console.log("activated")
+     },
+}
+</script>
+ 
